3a59ae14f2f9ddcff873123cb924d93c7b935b49
Ou4y <Ou4y43214321@gmail.com>
2025-12-23 02:24:56 +0200

    fix: set API key name in OpenAIKeyStore for secure storage

--- lib/features/ai_chat/services/openai_key_store.dart
+++ lib/features/ai_chat/services/openai_key_store.dart
@@ -2,7 +2,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class OpenAIKeyStore {
   static const _storage = FlutterSecureStorage();
-  static const _keyName = '';
+  static const _keyName = 'OPENAI_API_KEY';
 
   static Future<void> saveKey(String key) async {
     await _storage.write(key: _keyName, value: key);
